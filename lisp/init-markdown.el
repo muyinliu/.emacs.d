fdb7ca74fda2564dbe23a51cac9f986b8c7a9ffd
muyinliu <muyinliu@gmail.com>
2018-06-07 08:21:50 +0800

    update face of markdown-mode to make it efficient

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,6 +3,31 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (progn
+          (setq markdown-command "multimarkdown")
+          (setq markdown-fontify-code-blocks-natively t)
+          (add-hook 'markdown-mode-hook
+                    (lambda ()
+                      (setq buffer-face-mode-face '(:family "Monaco" :height 1.1))
+                      (buffer-face-mode))))
+  :config (custom-set-faces
+           '(markdown-header-face ((t (:inherit font-lock-function-name-face
+                                                :weight bold
+                                                :foreground "#1524FB"
+                                                :family "Menlo"))))
+           '(markdown-list-face ((t (:inherit markdown-markup-face :foreground "Purple"))))
+           '(markdown-code-face ((t (:inherit font-lock-function-name-face
+                                              :background "#FFFFEA"
+                                              :foreground "black"
+                                              :family "Monaco"))))
+           '(markdown-inline-code-face ((t (:inherit (markdown-code-face
+                                                      font-lock-constant-face)
+                                                     :height 0.9
+                                                     :background "gray95"
+                                                     :box (:line-width 1 :color "grey75")))))
+           '(markdown-bold-face ((t (:inherit bold
+                                              :weight bold
+                                              :height 1.1
+                                              :family "Menlo"))))))
 
 (provide 'init-markdown)
