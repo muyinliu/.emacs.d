362a57e8f7e312c321e862e34c2bdd0706e65154
muyinliu <muyinliu@gmail.com>
2018-06-08 18:10:28 +0800

    add faces for markdown-mode on console

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -10,24 +10,40 @@
                     (lambda ()
                       (setq buffer-face-mode-face '(:family "Monaco" :height 1.1))
                       (buffer-face-mode))))
-  :config (custom-set-faces
-           '(markdown-header-face ((t (:inherit font-lock-function-name-face
+  :config (if (console-p)
+              (custom-set-faces
+               '(markdown-header-face ((t (:inherit font-lock-function-name-face
+                                                    :weight bold
+                                                    :foreground "#1524FB"
+                                                    :family "Menlo"))))
+               '(markdown-list-face ((t (:inherit markdown-markup-face :foreground "Purple"))))
+               '(markdown-code-face ((t (:inherit font-lock-function-name-face
+                                                  :foreground "white"
+                                                  :family "Monaco"))))
+               '(markdown-inline-code-face ((t (:inherit (font-lock-constant-face)
+                                                         :foreground "#FF8000"
+                                                         :family "Monaco"))))
+               '(markdown-table-face ((t (:inherit (font-lock-constant-face)
+                                                   :foreground "white"))))
+               '(markdown-bold-face ((t (:inherit bold
+                                                  :weight bold
+                                                  :family "Menlo")))))
+            (custom-set-faces
+             '(markdown-header-face ((t (:inherit font-lock-function-name-face
+                                                  :weight bold
+                                                  :foreground "#1524FB"
+                                                  :family "Menlo"))))
+             '(markdown-list-face ((t (:inherit markdown-markup-face :foreground "Purple"))))
+             '(markdown-code-face ((t (:inherit font-lock-function-name-face
+                                                :foreground "black"
+                                                :family "Monaco"))))
+             '(markdown-table-face ((t (:inherit (font-lock-constant-face)
+                                                 :foreground "black"))))
+             '(markdown-inline-code-face ((t (:inherit (font-lock-constant-face)
+                                                       :foreground "#FF8000"
+                                                       :family "Monaco"))))
+             '(markdown-bold-face ((t (:inherit bold
                                                 :weight bold
-                                                :foreground "#1524FB"
-                                                :family "Menlo"))))
-           '(markdown-list-face ((t (:inherit markdown-markup-face :foreground "Purple"))))
-           '(markdown-code-face ((t (:inherit font-lock-function-name-face
-                                              :background "#FFFFEA"
-                                              :foreground "black"
-                                              :family "Monaco"))))
-           '(markdown-inline-code-face ((t (:inherit (markdown-code-face
-                                                      font-lock-constant-face)
-                                                     :height 0.9
-                                                     :background "gray95"
-                                                     :box (:line-width 1 :color "grey75")))))
-           '(markdown-bold-face ((t (:inherit bold
-                                              :weight bold
-                                              :height 1.1
-                                              :family "Menlo"))))))
+                                                :family "Menlo")))))))
 
 (provide 'init-markdown)
