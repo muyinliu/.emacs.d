d90bf95caeb16229d34f29bbe563550211c3b8f7
muyinliu <muyinliu@gmail.com>
2020-10-19 09:36:17 +0800

    remove useless config

--- init.el
+++ init.el
@@ -98,23 +98,3 @@
 (put 'downcase-region 'disabled nil)
 
 (provide 'init)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cfs--current-profile-name "same-width" t)
- '(cfs--fontsize-steps (quote (4 4 4)) t)
- '(package-selected-packages (quote (parinfer))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; Local Variables:
-;; coding: utf-8-emacs
-;; End:
-
